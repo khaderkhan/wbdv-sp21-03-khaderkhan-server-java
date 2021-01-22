7ac40ac90b0cdbf50e8d3a9396d19e06c35c5252
Mubaris Khan <mubariskhan@Mubariss-MacBook-Air.local>
2021-01-22 09:18:32 -0500

    adding the sign in page

---
+++ src/test/java/com/example/wbdvsp2103khaderkhanserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2103khaderkhanserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
