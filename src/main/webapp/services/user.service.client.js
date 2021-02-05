bdcedb1fb005feb2e7433844256478ec386f2888
Mubaris Khan <mubaris.khan786@gmail.com>
2021-02-05 00:29:18 -0500

    adding the services folder

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,40 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/khan.mubaris/users';
+    var self = this;
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(course)
+        }).then(function (response) {
+            return response.json()
+        })
+    }
+    function findAllUsers() {
+        return fetch(self.url)
+            .then(function (response) {
+                return response.json()
+            })
+    }
+    function findUserById(userId) { … }
+    function updateUser(userId, user) {
+        return fetch(`${self.url}/${userId}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(response => response.json())
+    }
+    function deleteUser(userId) {
+        return fetch(`${self.url}/${courseId}`,
+            {method: 'DELETE'})
+    }
+}
