cbf256e2a9d9f0ab6a309776729f081ffa917849
Mubaris Khan <mubaris.khan786@gmail.com>
2021-02-06 18:01:24 -0500

    saving progress to be picked up later on

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,18 +1,19 @@
 function AdminUserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
-    this.findUserById = findUserById;
+    //this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
     this.url = 'https://wbdv-generic-server.herokuapp.com/api/khan.mubaris/users';
     var self = this;
     function createUser(user) {
+        console.log("user here====>>", user)
         return fetch(self.url, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(course)
+            body: JSON.stringify(user)
         }).then(function (response) {
             return response.json()
         })
@@ -23,7 +24,7 @@ function AdminUserServiceClient() {
                 return response.json()
             })
     }
-    function findUserById(userId) { … }
+    //function findUserById(userId) { … }
     function updateUser(userId, user) {
         return fetch(`${self.url}/${userId}`, {
             method: 'PUT',
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,33 +1,110 @@
 (function () {
     var $usernameFld, $passwordFld;
     var $firstNameFld, $lastNameFld, $roleFld;
-    var $removeBtn, $editBtn, $createBtn;
+    var $removeBtn, $editBtn, $createUserBtn;
     var $userRowTemplate, $tbody;
     var userService = new AdminUserServiceClient();
+    var allUsers
     $(main);
+    const usersData = [];
 
     function main() {
-        $usernameFld = $('#usernameFld');
-        $passwordFld = $('#passwordFld')
-        $firstNameFld = $('#firstNameFld')
-        $lastNameFld = $('#lastNameFld')
-        $roleFld = $('')
-                            $removeBtn =
-                                $editBtn =
-                                    $createBtn =
-                                        $userRowTemplate =
-                                            $tbody = jQuery("tbody")
+        console.log("coming in main====>>>")
 
+        $createUserBtn = $('.wbdv-create-user');
 
+        $userRowTemplate = $('.wbdv-template');
+        $tbody = $('tbody')
+        // $removeBtn = $('')
+        // $editBtn = $('')
+        console.log("coming in this=====>>>")
+        $createUserBtn.click(createUser);
 
+        userService
+            .findAllUsers()
+            .then(renderUsers)
 
 
     }
-    function createUser() { … }
-    function deleteUser() { … }
-    function selectUser() { … }
-    function updateUser() { … }
-    function renderUsers(users) { … }
-    function findAllUsers() { … } // optional - might not need this
-    function findUserById() { … } // optional - might not need this
+    function createUser() {
+        // $createUserBtn.click(createUser);
+        //
+        // userService
+        //     .findAllUsers()
+        //     .then(renderUsers)
+
+        $usernameFld = $('#usernameFld')[0].value;
+        $passwordFld = $('#passwordFld')[0].value;
+        $firstNameFld = $('#firstNameFld')[0].value;
+        $lastNameFld = $('#lastNameFld')[0].value;
+        $roleFld = $('#roleFld')[0].value;
+
+        console.log("doneee", $usernameFld, $passwordFld, $firstNameFld, $lastNameFld, $roleFld)
+
+        var user = {
+            username: $usernameFld,
+            password: $passwordFld,
+            firstName: $firstNameFld,
+            lastName: $lastNameFld,
+            role: $roleFld
+        }
+
+        userService
+            .createUser(user)
+            .then(renderUsers)
+    }
+     function deleteUser(event) {
+
+     }
+     function selectUser(event) {
+         const selectBtn = jQuery(event.target)
+         const theId = selectBtn.attr("id")
+         console.log("courses====>>>", allUsers)
+
+         selectedCourse = allUsers.find(user => user._id === theId)
+         titleFld.val(selectedCourse.title)
+         $seatsFld.val(selectedCourse.seats)
+         $semesterFld.val(selectedCourse.semester)
+
+     }
+    // function updateUser() { … }
+    function renderUsers(users) {
+        $tbody.empty()
+        console.log("users here====>>>", users, users.length)
+        allUsers = users
+        for(let u in users) {
+            const user = users[u]
+            // console.log("user here is ===>>", user)
+            // const rowClone = $userRowTemplate.clone();
+            // rowClone.removeClass('wbdv-hidden');
+            // rowClone.find('.wbdv-username').html(user.username);
+            // $tbody.append(rowClone);
+
+
+            $tbody
+                .prepend(`
+     <tr>
+        <td>${user.username}</td>
+        <td>${user.password}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.role}</td>
+        <td>
+            <button class="wbdv-delete" id="${u}">Delete</button>
+            <button class="wbdv-select" id="${user._id}">Select</button>
+        </td>
+    </tr>
+  `)
+
+        }
+
+        $(".wbdv-delete")
+            .click(deleteUser)
+        $(".wbdv-select")
+            .click(selectUser)
+
+        console.log("doneeee")
+    }
+    // function findAllUsers() { … } // optional - might not need this
+    // function findUserById() { … } // optional - might not need this
 })();
